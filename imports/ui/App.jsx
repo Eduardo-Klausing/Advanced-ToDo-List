5589455b500712f349d7cb882649ab2f9b600c46
Eduardo-Klausing <eduardokgmuniz@gmail.com>
2025-04-24 09:38:01 -0300

    implementa telas de boas-vindas e tasks simplificadas

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,32 @@
 import React, { Fragment } from 'react';
-import { LoginForm } from './LoginForm';
 import { useTracker } from 'meteor/react-meteor-data';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { LoginForm } from './LoginForm';
+import { Welcome } from './Welcome';
+import { Task } from './Task';
 
 
 export const App = () => {
   const user = useTracker(() => Meteor.user());
 
   return (
-    <div className="main">
-        {user ? (
-          <Fragment>
-              <p>nada</p>
-          </Fragment>
-        ) : (
-          <LoginForm />
-        )}
-      </div>
+    <Routes>
+      <Route
+          path="/"
+          element={user ? <Navigate to="/welcome" /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <LoginForm /> : <Navigate to="/welcome" />}
+        />
+        <Route
+          path="/welcome"
+          element={user ? <Welcome /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/tasks"
+          element={user ? <Task /> : <Navigate to="/login" />}
+        />
+      </Routes>
   );
 };
---
+++ imports/ui/Task.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Task() {
+  return <h1>Lista de Tarefas</h1>;
+}
