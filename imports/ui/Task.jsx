71fc27d6956c95560067afe5cfdea5ebd1f6314c
Eduardo-Klausing <eduardokgmuniz@gmail.com>
2025-04-28 10:37:44 -0300

    implementa lista todo de acordo com instrucao 3

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import ToDoList from './ToDoList';
+import { ToDoList } from './ToDoList';
 
 export const Task = () => {
   const navigate = useNavigate();
 
+  const tarefas = [
+    { title: 'Comprar pão', user: 'Eduardo' },
+    { title: 'Estudar React', user: 'Rafael' },
+    { title: 'Lavar a louça', user: 'João' },
+  ];
+
   return (
     <div>
       <h1>Lista de Tarefas</h1>
-        <ToDoList />
+        <ToDoList tasks={tarefas} />
       <button onClick={() => Meteor.logout()}>Logout</button>
     </div>
   );
--- imports/ui/ToDoList.jsx
+++ imports/ui/ToDoList.jsx
@@ -2,39 +2,23 @@ import * as React from 'react';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-import ListItemAvatar from '@mui/material/ListItemAvatar';
-import Avatar from '@mui/material/Avatar';
-import ImageIcon from '@mui/icons-material/Image';
-import WorkIcon from '@mui/icons-material/Work';
-import BeachAccessIcon from '@mui/icons-material/BeachAccess';
+import ListItemIcon from '@mui/material/ListItemIcon'
+import InboxIcon from '@mui/icons-material/Inbox'
 
-export default function ToDoList() {
+export function ToDoList({ tasks }) {
   return (
-    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <ImageIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <WorkIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <BeachAccessIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Vacation" secondary="July 20, 2014" />
-      </ListItem>
+    <List>
+      {tasks.map((task, index) => (
+        <ListItem key={index}>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText
+            primary={task.title}
+            secondary={task.user}
+          />
+        </ListItem>
+      ))}
     </List>
   );
 }
