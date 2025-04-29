0f8615bc3939cc8dae34c33d4099f81b70b3c61f
Eduardo-Klausing <eduardokgmuniz@gmail.com>
2025-04-29 10:22:04 -0300

    implementa lista com funcionalidade de remover tarefas

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -4,17 +4,12 @@ import { ToDoList } from './ToDoList';
 
 export const Task = () => {
   const navigate = useNavigate();
-
-  const tarefas = [
-    { title: 'Comprar pão', user: 'Eduardo' },
-    { title: 'Estudar React', user: 'Rafael' },
-    { title: 'Lavar a louça', user: 'João' },
-  ];
+  
 
   return (
     <div>
       <h1>Lista de Tarefas</h1>
-        <ToDoList tasks={tarefas} />
+        <ToDoList />
       <button onClick={() => Meteor.logout()}>Logout</button>
     </div>
   );
