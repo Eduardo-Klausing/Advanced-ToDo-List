e7ccc4e49683d1991bf5883faf3af0a9839ee79b
Eduardo-Klausing <eduardokgmuniz@gmail.com>
2025-05-22 10:09:11 -0300

    etapa 7 - implementa campos das informações do usuário

--- imports/ui/Perfil.jsx
+++ imports/ui/Perfil.jsx
@@ -1,13 +1,45 @@
 import React from 'react';
+import { TextField, Stack, FormControl, InputLabel, Select, MenuItem, Avatar, Button } from '@mui/material';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+const handleSubmit = () => {
+    
+}
 
 export const Perfil = () => {
     return(
-    <div>
+    <Stack spacing={2}>
         <h1>Perfil de usuário</h1>
 
-        <form>
+        <TextField
+            label="Nome"
+        />
 
-        </form>
-    </div>
+        <TextField
+            label="Email"
+        />
+
+        <TextField
+            label="Data de Nascimento"
+        />
+
+        <FormControl>
+            <InputLabel>Gênero</InputLabel>
+            <Select>
+                <MenuItem value="Masculino">Masculino</MenuItem>
+                <MenuItem value="Feminino">Feminino</MenuItem>
+                <MenuItem value="Outro">Outro</MenuItem>
+            </Select>
+        </FormControl>
+
+
+        <TextField
+            label="Empresa"
+        />
+
+        <Button variant="contained" onClick={handleSubmit}>
+            Salvar
+        </Button>
+    </Stack>
     );
-}
+};
